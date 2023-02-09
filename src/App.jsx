f07b5b20ccf5518c87dbd064d0f3eb49c0cd8825
ArunRaj2136 <sakearun2126@gmail.com>
2023-02-10 02:37:39 +0530

    Baisc Layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./app.scss";
 
 function App() {
-  return <div>HAiii</div>;
+  return (
+    <div className="container">
+      <p className="title">find a free time</p>
+      <div className="inputs__container">
+        <input type="date"></input>
+        <input type="time"></input>
+        <input type="number"></input>
+      </div>
+      <button>Find</button>
+      <div className="timer__container"></div>
+      <div className="indicators__container"></div>
+    </div>
+  );
 }
 
 export default App;
